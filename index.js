4710c8b612cd176a61ccc2c76e59e5d59eb269f6
safaziedi <safaziedi070@gmail.com>
2024-02-16 13:54:01 +0100

    Charger les données et démarrer le serveur

--- index.js
+++ index.js
@@ -1,20 +1,8 @@
 const express = require('express')
 const app = express()
 const port = 3000;
-
-/*
-    LOGGER
- */
-const logger  = (req, res, next) => {
-    console.log("Logger started ...")
-    console.log(req.url)
-    next()
-}
-
-app.get('/about', logger, (req, res) => {
-    return res.send('About Page')
-})
-
+//Charger les données et démarrer le serveur
+const products = require('./data.js')
 
 //les api 
 app.get('/', (req, res) => {
