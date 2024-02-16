6cb5569277836b7d8633b09bb01f9f9face5f228
safaziedi <safaziedi070@gmail.com>
2024-02-16 14:10:06 +0100

    update

--- index.js
+++ index.js
@@ -36,6 +36,23 @@ app.get('/api/products/:productID', (req, res) => {
     res.json(product)
 })
 
+
+app.put('/api/products/:productID', (req, res) => {
+    const id = Number(req.params.productID)
+    const index = products.findIndex(product => product.id === id)
+    if (index === -1) {
+        return res.status(404).send('Product not found')
+    }
+    const updatedProduct = {
+        id: products[index].id,
+        name: req.body.name,
+        price: req.body.price
+    }
+    products[index] = updatedProduct
+    res.status(200).json('Product updated')
+})
+
+
 app.listen(port, () => {
     console.log(`listening at http://localhost:${port}`);
 })
