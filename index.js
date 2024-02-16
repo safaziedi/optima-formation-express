a036bd7d33a954ab361878ea85cf58e490ade5b6
safaziedi <safaziedi070@gmail.com>
2024-02-16 13:49:04 +0100

    data json

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
 const port = 3000;
+
+/*
+    LOGGER
+ */
+const logger  = (req, res, next) => {
+    console.log("Logger started ...")
+    console.log(req.url)
+    next()
+}
+
+app.get('/about', logger, (req, res) => {
+    return res.send('About Page')
+})
+
+
 //les api 
 app.get('/', (req, res) => {
     res.send('Hello OPTIMA')
