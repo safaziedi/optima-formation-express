dcadb0e87e30de63d1ecab465a871ea01e9895b6
safaziedi <safaziedi070@gmail.com>
2024-02-16 13:59:00 +0100

    Create: POST /api/products

--- index.js
+++ index.js
@@ -8,6 +8,21 @@ const products = require('./data.js')
 app.get('/', (req, res) => {
     res.send('Hello OPTIMA')
 })
+
+
+app.use(express.json()) // parse json body content
+
+app.post('/api/products', (req, res) => {
+    const newProduct = {
+        id: products.length + 1,
+        name: req.body.name,
+        price: req.body.price
+    }
+    products.push(newProduct)
+    res.status(201).json(newProduct)
+})
+
+
 app.listen(port, () => {
     console.log(`listening at http://localhost:${port}`);
 })
