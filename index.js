1f41c05f8c5b02bd81e81cf6dcc4868a05eef826
safaziedi <safaziedi070@gmail.com>
2024-02-16 14:07:17 +0100

    get by id

--- index.js
+++ index.js
@@ -26,6 +26,16 @@ app.get('/api/products', (req, res) => {
     res.json(products)
 })
 
+app.get('/api/products/:productID', (req, res) => {
+    const id = Number(req.params.productID)
+    const product = products.find(product => product.id === id)
+
+    if (!product) {
+        return res.status(404).send('Product not found')
+    }
+    res.json(product)
+})
+
 app.listen(port, () => {
     console.log(`listening at http://localhost:${port}`);
 })
