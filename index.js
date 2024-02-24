a5254b9cc1064a4ad3a20aec6035575ee8a4b9b7
safaziedi <safaziedi070@gmail.com>
2024-02-24 11:48:40 +0100

    crud + auth module user only

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ const mongoose = require('mongoose');
 const morgan = require('morgan');
 
 const port = 3000;
-//Charger les données
-const products = require('./data.js')
 
 /*
 MIDDELWAREs
@@ -18,9 +16,7 @@ app.disable('x-powered-by');//pour réduire les informations divulguées dans le
 /*
 LES API
  */
-
-// Use the productRoutes for the '/product' path
-// app.use('/product', productRoutes);
+//CRUD USER
 const userRoutes = require('./routes/UserRoutes');
 app.use('/user', userRoutes);
 
@@ -30,71 +26,6 @@ app.use('/user', userRoutes);
 app.get('/', (req, res) => {
     res.send('Hello OPTIMA')
 })
-
-
-
-/*
-CRUD
- */
-app.use(express.json()) // parse json body content
-
-app.post('/api/products', (req, res) => {
-    const newProduct = {
-        id: products.length + 1,
-        name: req.body.name,
-        price: req.body.price
-    }
-    products.push(newProduct)
-    res.status(201).json(newProduct)
-})
-
-app.get('/api/products', (req, res) => {
-    res.json(products)
-})
-
-app.get('/api/products/:productID', (req, res) => {
-    const id = Number(req.params.productID)
-    const product = products.find(product => product.id === id)
-
-    if (!product) {
-        return res.status(404).send('Product not found')
-    }
-    res.json(product)
-})
-
-
-app.put('/api/products/:productID', (req, res) => {
-    const id = Number(req.params.productID)
-    const index = products.findIndex(product => product.id === id)
-    if (index === -1) {
-        return res.status(404).send('Product not found')
-    }
-    const updatedProduct = {
-        id: products[index].id,
-        name: req.body.name,
-        price: req.body.price
-    }
-    products[index] = updatedProduct
-    res.status(200).json('Product updated')
-})
-
-
-
-app.delete('/api/products/:productID', (req, res) => {
-    const id = Number(req.params.productID)
-    const index = products.findIndex(product => product.id === id)
-        if (index === -1) {
-        return res.status(404).send('Product not found')
-    }
-    products.splice(index,1)
-    res.status(200).json('Product deleted')
-})
-
-
-
-/*
-Authentication
-*/
 
 
 /*
